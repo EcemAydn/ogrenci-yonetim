6b95792de57ddeac18ae5f48dbd3e5f66695f6c8
EcemAydn <ecemaydn15@gmail.com>
2023-02-22 12:08:42 +0300

    update fonksiyonu düzenlendi

--- src/stores/student.js
+++ src/stores/student.js
@@ -4,24 +4,41 @@ import { defineStore } from 'pinia';
 export const useStudentStore = defineStore('student', () => {
     const students = ref([]);
 
-    if(localStorage.getItem('students')){
-        students.value = JSON.parse(localStorage.getItem('students'));
-    }
+    // function findStudent(id){
+    //     const findStd = students.value.findIndex(s => s.id === id)
+    //     console.log(findStd);
+    // }
 
     // const filteredStudents =  computed(() => {
-        //     return students.value.filter((std) => std.name === 'ecem');
-        // });
+    //     return students.value.filter((std) => std.id === id);
+    // });
+
+    function getStudents() {
+        if(localStorage.getItem('students')){
+            students.value = JSON.parse(localStorage.getItem('students'));
+        }
+    }
+
+    function getStudentById(id) {
+        return students.value.find((a) => a.id === id);;
+    }
         
     function addStudent(item) {
         students.value.push(item);
-        localStorage.setItem('students', JSON.stringify(students.value))      
-
+        localStorage.setItem('students', JSON.stringify(students.value));
     }
-        
-        // function deleteStudent(id) {
-            //     const findIndex = students.value.findIndex((j)=> j.id === id);
-            //     students.value.splice(findIndex, 1);
-            // }
-    return {students, addStudent}
-    // return { students, addStudent, deleteStudent, filteredStudents };
+
+    function updateStudent(student){
+        const findStd = students.value.findIndex((a) => a.id === student.id);
+        students.value.splice(findStd, 1 , student);
+        localStorage.setItem('students', JSON.stringify(students.value));
+    }
+
+    function deleteStudent(id) {
+        const findIndex = students.value.findIndex((j)=> j.id === id);
+        students.value.splice(findIndex, 1);
+        localStorage.setItem('students', JSON.stringify(students.value));
+     }
+
+    return { students, getStudents, getStudentById, addStudent, deleteStudent, updateStudent}
 });
