cf220283b7d41319120cdea8f91b684d50ac50b9
EcemAydn <ecemaydn15@gmail.com>
2023-02-22 12:09:57 +0300

    sayfa düzenlemesi

--- src/views/Listele.vue
+++ src/views/Listele.vue
@@ -14,82 +14,82 @@ import { useStudentStore } from '@/stores/student';
 <template>
   <div class="w-full h-full flex flex-col items-center">
     <h1 lang="tr-TR" class="text-2xl">Öğrenci Listesi</h1>
-    <div class="border p-4 rounded-md m-8 w-fit overflow-hidden h-full shadow-lg">
-      <div class="flex justify-end">
+    <div class="border p-4 rounded-md m-8 w-full overflow-hidden h-full shadow-lg">
+      <div class="flex justify-between">
+        <input type="search" class="w-1/2 border rounded-md outline-none p-1">
         <router-link class="bg-green-500 p-1 pl-4 pr-4 rounded-md text-white shadow-inner hover:shadow-none hover:bg-green-600 hover:transition hover:duration-500" :to="{ name: 'OlusturSayfasi' }">
           + Yeni kayıt oluştur
         </router-link>
       </div>
-      <div class="flex flex-col">
-        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8 h-full">
-          <div class="py-2 inline-block min-w-full relative sm:px-6 lg:px-8 h-full">
-            <div class="h-full overflow-y-auto">
-              <table class="min-w-full h-full">
-                <thead class="border-b bg-gray-800 sticky top-0 left-0 z-10">
-                  <tr>
-                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                      Ad
-                    </th>
-                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                      Soyad
-                    </th>
-                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                      Aile Telefonu
-                    </th>
-                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                      Öğrenci Numarası
-                    </th>
-                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                      Sınıfı
-                    </th>
-                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                      Sınıf Öğretmeni
-                    </th>
-                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                      Düzenle
-                    </th>
-                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                      Sil
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="">
-                  <tr
-                    v-for="student in studentStore.students" 
-                    :key="student.id"
-                    class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {{ student.ad }}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {{ student.soyad }}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {{ student.tel }}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {{ student.numara }}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {{ student.sinif }}               
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {{ student.ogr }}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      <router-link
-                        :to="{ name:'GuncellemeSayfasi' }"
-                        class="bg-blue-500 text-white p-1 pl-4 pr-4 shadow-inner rounded-md hover:shadow-lg" 
-                        >Düzenle</router-link>
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                       <button class="bg-red-500 text-white p-1 pl-4 pr-4 shadow-inner rounded-md hover:shadow-lg" @click="deleteButton(student.id)">Sil</button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+
+      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8 h-full px-6 pt-4 pb-6">
+        <div class="h-full w-full overflow-auto">
+          <table class="min-w-full">
+            <thead class="border-b bg-gray-800 sticky top-0 left-0 z-10">
+              <tr>
+                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+                  Ad
+                </th>
+                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+                  Soyad
+                </th>
+                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+                  Aile Telefonu
+                </th>
+                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+                  Öğrenci Numarası
+                </th>
+                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+                  Sınıfı
+                </th>
+                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+                  Sınıf Öğretmeni
+                </th>
+                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+                  Düzenle
+                </th>
+                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+                  Sil
+                </th>
+              </tr>
+            </thead>
+
+            <tbody class="">
+              <tr
+                v-for="student in studentStore.students" 
+                :key="student.id"
+                class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100 even:bg-gray-50 last:border-b-0"
+              >
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {{ student.ad }}
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {{ student.soyad }}
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {{ student.tel }}
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {{ student.numara }}
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {{ student.sinif }}               
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {{ student.ogr }}
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  <router-link
+                    :to="{ name:'GuncellemeSayfasi', params:{ id: student.id} }"
+                    class="bg-blue-500 text-white p-1 pl-4 pr-4 shadow-inner rounded-md hover:shadow-lg" 
+                    >Düzenle</router-link>
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <button class="bg-red-500 text-white p-1 pl-4 pr-4 shadow-inner rounded-md hover:shadow-lg" @click="deleteButton(student.id)">Sil</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
